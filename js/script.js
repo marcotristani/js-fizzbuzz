151347efc40b0d9b86771e9503934d2b4454d86c
marcotristani <marco.tristani@hotmail.it>
2025-12-02 13:05:37 +0100

    flat: ciclo numeri da 1 a 100

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 // FizzBuzz
 //Fin quando non arrivo a 100 scrivo i numeri uno alla volta incrementandoli di uno
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+    
+}
   // se il numero è divisibile per 3 scrivo fizz al posto del numero( ma non deve essere divisibile per 5 anche)
   // se il numero è divisibile per 5 scrivo il buzz al posto del numero( ma non deve essere divisibile per 3 anche)
   // se il numero è divisibile per 3 e allo stesso tempo è divisibile per 5 allora scrivo fizzbuzz al posto del numero
