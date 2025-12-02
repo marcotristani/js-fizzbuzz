7663f3e934b42bc35ee93e8b4f20c88c0773b051
marcotristani <marco.tristani@hotmail.it>
2025-12-02 13:00:48 +0100

    flat: inizializzazione pagine e logica programma

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// FizzBuzz
+//Fin quando non arrivo a 100 scrivo i numeri uno alla volta incrementandoli di uno
+  // se il numero è divisibile per 3 scrivo fizz al posto del numero( ma non deve essere divisibile per 5 anche)
+  // se il numero è divisibile per 5 scrivo il buzz al posto del numero( ma non deve essere divisibile per 3 anche)
+  // se il numero è divisibile per 3 e allo stesso tempo è divisibile per 5 allora scrivo fizzbuzz al posto del numero
