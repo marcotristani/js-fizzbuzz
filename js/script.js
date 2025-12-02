66dfc78aac991ab464026e6474d2f2f7de20674a
marcotristani <marco.tristani@hotmail.it>
2025-12-02 13:29:35 +0100

    flat: condizione divisibile per 5 e per 3 allo stesso tempo

--- js/script.js
+++ js/script.js
@@ -6,14 +6,16 @@ for (let i = 1; i <= 100; i++) {
     if ( i % 3 === 0 && i % 5 !== 0 ) {
         alternative = "Fizz";
         console.log(alternative);
-    }else if( i % 5 === 0 && i % 3 !== 0 ){
+    }else if( i % 5 === 0 && i % 3 !== 0 ){// se il numero è divisibile per 5 scrivo il buzz al posto del numero( ma non deve essere divisibile per 3 anche)
         alternative = "Buzz";
         console.log(alternative);
+    }else if( i % 3 === 0 && i % 5 === 0){
+        alternative = "BuzzFizz";
+        console.log(alternative);
     }else {
-        console.log(i);
-        
+        console.log(i)
     }
 }
   
-  // se il numero è divisibile per 5 scrivo il buzz al posto del numero( ma non deve essere divisibile per 3 anche)
+  
   // se il numero è divisibile per 3 e allo stesso tempo è divisibile per 5 allora scrivo fizzbuzz al posto del numero
