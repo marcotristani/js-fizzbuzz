e03776190dee07d9ed7e9431f88200753e3cb666
marcotristani <marco.tristani@hotmail.it>
2025-12-02 13:09:22 +0100

    flat: condizione divisibile per 3

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 // FizzBuzz
 //Fin quando non arrivo a 100 scrivo i numeri uno alla volta incrementandoli di uno
+let alternative;
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
-    
+    // se il numero è divisibile per 3 scrivo fizz al posto del numero( ma non deve essere divisibile per 5 anche)
+    if ( i % 3 === 0 ) {
+        alternative = "fizz";
+        console.log(alternative);
+    }else{
+        console.log(i);
+        
+    }
 }
-  // se il numero è divisibile per 3 scrivo fizz al posto del numero( ma non deve essere divisibile per 5 anche)
+  
   // se il numero è divisibile per 5 scrivo il buzz al posto del numero( ma non deve essere divisibile per 3 anche)
   // se il numero è divisibile per 3 e allo stesso tempo è divisibile per 5 allora scrivo fizzbuzz al posto del numero
